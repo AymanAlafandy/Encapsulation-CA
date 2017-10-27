6080a6a6b9d5c0258c20b9c489a8ca3bace939a3
bhanu18v <bhanu18v@gmail.com>
2017-10-27 11:54:12 +0200

    First Part of Encapsulation Done

---
+++ Encapsulate/Encapsulate/PersonHandler.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+ public class PersonHandler
+    {
+        public void SetAge(Person per,int age)
+        {
+            per.Age = age;
+        }
+
+        public void SetFName(Person per, string fName)
+        {
+            per.FName = fName;
+        }
+        public void SetLName(Person per, string lName)
+        {
+            per.LName = lName;
+        }
+        public void SetHeight(Person per, double height)
+        {
+            per.Height = height;
+        }
+        public void SetWeight(Person per, double weight)
+        {
+            per.Weight = weight;
+        }
+
+
+        public int GetAge(Person per)
+        {
+            return per.Age;
+        }
+
+        public string GetFName(Person per)
+        {
+            return per.FName;
+        }
+        public string GetLName(Person per)
+        {
+            return per.LName;
+        }
+        public double GetHeight(Person per)
+        {
+            return per.Height;
+        }
+        public double GetWeight(Person per)
+        {
+            return per.Weight;
+        }
+
+
+
+
+
+        public Person CreatePerson(int age,string fName,string lName,double height,double weight)
+        {
+            Person bob = new Person();
+            bob.Age = age;
+            bob.FName = fName;
+            bob.LName = lName;
+            bob.Height = height;
+            bob.Weight = weight;
+
+
+            return bob;
+        }
+
+        public string PrintPerson(Person per)
+        {
+            return "Name: " + per.FName + " " + per.LName +
+                "\nAge: " + per.Age + "\nHeight: " + per.Height +
+                "\nWeight: " + per.Weight;
+
+        }
+
+
+    }
+}
--- Encapsulate/Encapsulate/Program.cs
+++ Encapsulate/Encapsulate/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Encapsulate
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            PersonHandler ph = new PersonHandler();
+            List<Person> personList = new List<Person>();
+            personList.Add(ph.CreatePerson(18, "Billy", "Ceaser", 170, 280));
+            personList.Add(ph.CreatePerson(33, "Bhanu", "vakamalla", 150, 58));
+            personList.Add(ph.CreatePerson(36, "Sai", "Bodimallu", 170, 70));
+            personList.Add(ph.CreatePerson(5, "Tanishca", "Bodimallu", 120, 25));
+            personList.Add(ph.CreatePerson(1, "Varnica", "Bodimallu", 80, 12));
+
+            foreach  (Person p in personList)
+            {
+                Console.WriteLine(ph.PrintPerson(p));
+            }
+
+            Console.ReadLine();
         }
     }
 }
