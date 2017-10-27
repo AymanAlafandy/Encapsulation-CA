bf401b63d14336a34f8dfbd49e85a04cb852999a
priya tadi <priya95tadi@hotmail.com>
2017-10-27 17:02:58 +0200

    ending

---
+++ Encapsulate/Encapsulate/inheritance/Hedgehog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    public class Hedgehog : Animal
+    {
+        public int NrOfRings { get; set; }
+
+        public Hedgehog() : base() { }
+
+        public Hedgehog(bool isCarivore, int age,
+            string species, int nrOfRings) :
+            base(isCarivore, age, species)
+        {
+            NrOfRings = nrOfRings;
+        }
+        public override string Stats()
+        {
+            return base.Stats() + "\nNr of rings:" + NrOfRings;
+        }
+    }
+}
---
+++ Encapsulate/Encapsulate/inheritance/Horse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    public class Horse : Animal
+    {
+        public bool IsFireBreathing { get; set; }
+
+        public Horse() : base() { }
+
+        public Horse(bool isCarnivore,int age, 
+            string species, bool isFireBreathing ):
+            base(isCarnivore, age,species)
+        {
+            IsFireBreathing = isFireBreathing;
+        }
+        public override string Stats()
+        {
+            return base.Stats() + "\nCan breath fire:" + IsFireBreathing;
+        }
+
+    }
+}
