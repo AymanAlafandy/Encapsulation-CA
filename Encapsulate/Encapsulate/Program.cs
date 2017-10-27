62998ebaeebac495785f403498735b51117a91b3
H F <jonis88@gmail.com>
2017-10-27 11:53:25 +0200

    First part done

--- Encapsulate/Encapsulate/Program.cs
+++ Encapsulate/Encapsulate/Program.cs
@@ -10,6 +10,21 @@ namespace Encapsulate
     {
         static void Main(string[] args)
         {
+
+            PersonHandler ph = new PersonHandler();
+            List<Person> personList = new List<Person>();
+
+            personList.Add(ph.CreatePerson(18, "Billy", "Ceasar", 170, 280));
+            personList.Add(ph.CreatePerson(18, "Carl", "Ceasar", 175, 280));
+            personList.Add(ph.CreatePerson(18, "Julle", "Ceasar", 170, 290));
+            personList.Add(ph.CreatePerson(18, "Gun", "Ceasar", 160, 240));
+            personList.Add(ph.CreatePerson(18, "Bill", "Ceasar", 169, 279));
+
+            foreach (Person p in personList)
+            {
+                Console.WriteLine(ph.PrintPerson(p));
+            }
+            Console.ReadKey();
         }
     }
 }
