e1c2f1e66ecbf2d3cb4705aa793c8614c4c5a8c3
John Hellman <acoppero@gmail.com>
2017-10-27 11:53:22 +0200

    First part done

--- Encapsulate/Encapsulate/Program.cs
+++ Encapsulate/Encapsulate/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Encapsulate
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+            PersonHandler ph = new PersonHandler();
+            List<Person> personList = new List<Person>();
+
+            personList.Add(ph.CreatePerson(lName:"Ceasar", fName:"Billy", age: 18, weight:  280,height: 170));
+            personList.Add(ph.CreatePerson(90,"Karl", "Ceasar", 190, 70));
+            personList.Add(ph.CreatePerson(8, "Ricard", "O'lamp",weight: 80));
+            personList.Add(ph.CreatePerson(180, "Old", "Man", 130, 60));
+            personList.Add(ph.CreatePerson(37, "Brark", "Hoblama", 190, 110));
+
+            foreach(Person p in personList)
+            {
+                Console.WriteLine(ph.PrintPerson(p));
+            }
+            Console.ReadLine();
+
         }
     }
 }
