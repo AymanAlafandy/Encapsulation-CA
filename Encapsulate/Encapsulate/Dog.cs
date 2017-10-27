b63e10ff41808e1d1113c94980d9b6005b707a5e
bhanu18v <bhanu18v@gmail.com>
2017-10-27 14:19:52 +0200

    Ineritance

---
+++ Encapsulate/Encapsulate/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+   public class Dog : Animal
+    {
+        public int TailWagsPerSecond { get; set; }
+
+        public Dog() : base() { }
+        public Dog(bool isCarnivore,int age,string species,int tailWagsPerSecond) : base(isCarnivore, age, species)
+        {
+            TailWagsPerSecond = tailWagsPerSecond;
+        }
+
+    }
+}
---
+++ Encapsulate/Encapsulate/Flamingo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+  public  class Flamingo: Bird
+    {
+        public bool NrOfLifePartners { get; set; }
+        public Flamingo() : base() { }
+
+        public Flamingo(bool isCarnivore,int age,string species,int singingDb,bool nrOfLifePartners) : base(isCarnivore, age, species, singingDb)
+        {
+            NrOfLifePartners = nrOfLifePartners;
+        }
+
+    }
+}
