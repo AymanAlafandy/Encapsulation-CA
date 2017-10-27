5b0f2411bbd8dfdca44d311b0704290818440591
ayman alafandy <myprogram@barid.com>
2017-10-27 16:57:56 +0200

    The End Ayman Aalfandy polymorphism

---
+++ Encapsulate/Encapsulate/CUserError.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    public abstract class CUserError
+    {
+
+
+        public abstract string UEMessage()
+        {
+            return "I Be LKing";
+        }
+    }
+}
---
+++ Encapsulate/Encapsulate/NumericInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    public class NumericInputError: CUserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a numeric input in a text only field";
+        }
+    }
+}
