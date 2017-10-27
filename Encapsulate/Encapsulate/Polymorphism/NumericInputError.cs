cd419b3d3279741892da741f1a6c7e66d35af369
huongngt <huongnt.x86@gmail.com>
2017-10-27 16:57:09 +0200

    complete part4

---
+++ Encapsulate/Encapsulate/Polymorphism/NumericInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "NumericInputError";
+        }
+    }
+}
---
+++ Encapsulate/Encapsulate/Polymorphism/UserError.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    public abstract class UserError
+    {
+        public abstract string UEMessage(); //abstract contain incompete function
+
+        public int ErrorCode; //abstract contain normal field, property
+
+        public virtual string King() //abstract contain normal function
+        {
+            return "aaaa";
+        } 
+
+
+    }
+}
