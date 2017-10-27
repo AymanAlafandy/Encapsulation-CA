100764581c5a7b81e4fc21b6ee9ae9a3a3970320
ayman alafandy <myprogram@barid.com>
2017-10-27 11:59:05 +0200

    TeacherAyman20171027(2)

---
+++ Encapsulate/Encapsulate/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    public class Person
+    {
+
+        //FIELDS
+        private int _age;
+        //
+        private string _fName;
+        //
+        private string _lName;
+        //
+        private double _height;
+        //
+        private double _weight;
+        
+        
+        //PROPS
+        public int Age
+        {
+            set { _age = value; }
+            get { return _age; }
+        }
+        //
+        public string FName
+        {
+            set { _fName = value; }
+            get { return _fName; }
+        }
+        //
+        public string LName
+        {
+            set { _lName = value; }
+            get { return _lName; }
+        }
+        //
+        public double Height
+        {
+            set { _height = value; }
+            get { return _height; }
+        }
+        //
+        public double Weight
+        {
+            set { _weight = value; }
+            get { return _weight; }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
--- Encapsulate/Encapsulate/Program.cs
+++ Encapsulate/Encapsulate/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Encapsulate
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            PersonHandler ph = new PersonHandler();
+            List<Person> personlist = new List<Person>();
+
+            personlist.Add(ph.CreatePerson(18, "Ayman0", "Alaafandy0", 178, 80));
+            personlist.Add(ph.CreatePerson(19, "Ayman1", "Alaafandy1", 179, 81));
+            personlist.Add(ph.CreatePerson(20, "Ayman2", "Alaafandy2", 180, 82));
+            personlist.Add(ph.CreatePerson(21, "Ayman3", "Alaafandy3", 181, 83));
+            personlist.Add(ph.CreatePerson(22, "Ayman4", "Alaafandy4", 182, 84));
+
+            foreach (Person p in personlist)
+            {
+                Console.WriteLine(ph.PrintPerson(p));
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
